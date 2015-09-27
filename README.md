dce941eacddf6a23c7d3eb15ff8dded47c1f953e
Rachel Munson <rmunson@ufl.edu>
2015-09-27 10:15:49 -0400

    changed flavor of ice cream to "vanilla"

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# URL
+# URL https://gist.github.com/racmuns/cbd8d233e9b6cca78bc3
 
 # Resources
+http://git-scm.com/videos
+http://www.lynda.com/GitHub-tutorials/Version-Control-Everyone/378045-2.html
+http://git-scm.com/video/what-is-version-control
 *
---
+++ instructions.js
@@ -0,0 +1,104 @@
+// 4 . Create	a	new	Git	branch	titled	“arrays”	and	add	these	changes:
+// a Create	a	variable	named	iceCream	and	assign	to	this	variable	an	array	of	three	
+// strings	which	are	the	names	of	your	top	three	favorite	ice	cream	flavors
+var iceCream = [
+	'rocky road',
+	'java nut',
+	'chocolate chip'
+	];
+
+// b On	a	new	line,	without	creating	a	new	variable,	add	a	fourth	string	of	your	fourth	
+// favorite	ice	cream	flavor to	the	iceCream	variable
+
+// 5 Create	a	new	Git	branch	titled	“presidents”	and	add	these	changes:
+	// a. Create	an	object	with	the	following	properties:
+	// d. Put	all	five	of	these	objects	into	an	array	and	assign	this	array	to	a	variable	named	
+		// “presidents”
+
+var presidents = [
+	{firstName:"Barack",
+	lastName:"Obama",
+	termLength:"Two terms - Incumbent",
+	party:"Democrat",
+	years:"8 Years",
+}
+
+	{firstName:"George",
+	lastName:"Bush",
+	termLength:"Two terms",
+	party:"Repubilcan",
+	years:"8 Years",
+}
+
+	{firstName:"Bill",
+	lastName:"Clinton",
+	terms:"Two terms",
+	party:"Democrat",
+	years"8 Years",
+}
+
+	{firstName:"George",
+	lastName:"Bush Sr.",
+	termLength:"One term",
+	party:"Requblican",
+	years:"4 Years",
+}
+
+	{firstName:"Ronald",
+	lastName:"Reagan",
+	termLength:"Two terms",
+	party:"Repubilcan",
+	years:"8 Years",
+}
+]
+
+
+
+// e. Use	console.log(),	the	presidents	variable,	and	array	notation	to	log	out	the	first	and	
+// last	name	of	the	third	president	(this	should	log	out	“Bill	Clinton”)
+
+function myFunction() {
+	console.log(presidents [2].lastName.firstName);
+}
+
+myFunction [3];
+
+var message = howdy('Rachel Munson', 'Hola!');
+	console.log(message);
+
+var numba = 7;
+
+if (string , numba) {
+	console.log('What a short little word!');
+}
+else if(string > numba) {
+	console.log('I am sorry, but that is too many to count');
+}
+else if(string = numba) {
+	console.log('What a perfect choice!');
+}
+
+
+
+function inception (callback, value) {
+	callback (value);
+}
+
+
+function numba(string) {
+	printToConsole("7")
+	printToConsole("seventy")
+	printToConsole("university")
+}
+
+
+
+
+
+
+
+
+
+
+
+

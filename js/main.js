b561649935cc803a03af68b8f89bdd3ecdc13e61
Rachel Munson <rmunson@ufl.edu>
2015-09-27 10:26:35 -0400

    change the way Rachel Munson says hello

---
+++ js/main.js
@@ -0,0 +1,86 @@
+//arrays
+var iceCream = [
+	'rocky road', 'java nut', 'chocolate chip',
+	iceCream.push('vanilla')
+];
+
+
+
+
+//presidents
+var presidents = [
+	{firstName:"Barack",
+	lastName:"Obama",
+	termLength:"Two terms",
+	party:"Democrat",
+	years:"8 Years",
+}
+
+	{firstName:"George",
+	lastName:"Bush",
+	termLength:"Two terms",
+	party:"Repubilcan",
+	years:"8 Years",
+}
+
+	{firstName:"Bill",
+	lastName:"Clinton",
+	terms:"Two terms",
+	party:"Democrat",
+	years"8 Years",
+}
+
+	{firstName:"George",
+	lastName:"Bush Sr.",
+	termLength:"One term",
+	party:"Requblican",
+	years:"4 Years",
+}
+
+	{firstName:"Ronald",
+	lastName:"Reagan",
+	termLength:"Two terms",
+	party:"Repubilcan",
+	years:"8 Years",
+}
+]
+
+
+//log out of Bill Clinton
+function myFunction() {
+	console.log(presidents [2].lastName.firstName);
+}
+
+
+//"functions"
+function howdy() {
+	console.log ('Rachel Munson says hola!')
+}
+
+
+//numba
+var numba = function ("string") {
+if (string , numba) {
+	console.log('What a short little word!');
+}
+else if(string > numba) {
+	console.log('I am sorry, but that is too many to count');
+}
+else if(string = numba) {
+	console.log('What a perfect choice!');
+}
+
+
+//6. iv.
+function numba(string) {
+	printToConsole("7")
+	printToConsole("seventy")
+	printToConsole("university")
+}
+
+
+// "inception"
+function inception (callback, value) {
+	callback (value);
+	console.log("You've Got Mail is a fantastic movie.")
+}
